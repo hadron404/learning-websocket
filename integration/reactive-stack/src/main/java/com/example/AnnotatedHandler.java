dc1b9c009249ed29b6febeb9a4acf23f9de1db35
zhouqiang <2902511578@qq.com>
2023-09-28 10:04:16 +0800

    how-to:使用SpringBoot技术栈构建一个reactive-stack的WebSocket服务端应用
    优化代码结构

--- integration/reactive-stack/src/main/java/com/example/AnnotatedHandler.java
+++ integration/reactive-stack/src/main/java/com/example/AnnotatedHandler.java
@@ -4,15 +4,13 @@ import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 
-public class InboundHandler implements WebSocketHandler {
+@WSRequestMapping("/anno")
+public class AnnotatedHandler implements WebSocketHandler {
 
 	@Override
 	public Mono<Void> handle(WebSocketSession session) {
-		// access the stream of inbound messages
 		return session.receive()
-			// do sth with each message
 			.doOnNext(m -> System.out.println(m.getPayloadAsText()))
-			// return a Mono<Void> that completes when receiving completes
 			.then();
 	}
 }
--- integration/reactive-stack/src/main/java/com/example/UseWebFluxConfiguration.java
+++ integration/reactive-stack/src/main/java/com/example/UseWebFluxConfiguration.java
@@ -2,21 +2,50 @@ package com.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ApplicationObjectSupport;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Configuration
-public class UseWebFluxConfiguration {
+public class UseWebFluxConfiguration extends ApplicationObjectSupport {
 
+	/**
+	 * 手动配置 Websocket Endpoint
+	 */
 	@Bean
-	public HandlerMapping handlerMapping() {
+	public HandlerMapping basicHandlerMapping() {
 		Map<String, WebSocketHandler> map = new HashMap<>();
-		map.put("/path", new InboundAndOutboundHandler());
+		map.put("/inbound", new InboundHandler());
+		map.put("/inout", new InboundAndOutboundHandler());
 		int order = -1; // before annotated controllers
 		return new SimpleUrlHandlerMapping(map, order);
+	}
+
+	/**
+	 * 基于注解 {@link WSRequestMapping} 自动配置 Websocket Endpoint
+	 */
+	@Bean
+	public HandlerMapping annotatedHandlerMapping() {
+		final Map<String, WebSocketHandler> urlMap = new LinkedHashMap<>();
+		Map<String, Object> beanMap = obtainApplicationContext().getBeansWithAnnotation(WSRequestMapping.class);
+		beanMap.values()
+			.forEach(bean -> {
+				if (!(bean instanceof WebSocketHandler)) {
+					throw new RuntimeException(
+						String.format("Class [%s] doesn't implement WSRequestMapping interface.",
+							bean.getClass().getName()));
+				}
+				WSRequestMapping annotation = AnnotationUtils.getAnnotation(bean.getClass(), WSRequestMapping.class);
+				urlMap.put(Objects.requireNonNull(annotation).value(), (WebSocketHandler) bean);
+			});
+		return new SimpleUrlHandlerMapping(urlMap, Ordered.HIGHEST_PRECEDENCE);
 	}
 }
