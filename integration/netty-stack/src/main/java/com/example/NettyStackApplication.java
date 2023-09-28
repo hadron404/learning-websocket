438c83a9ed4a99747438ecad77d0046755fc54bd
zhouqiang <2902511578@qq.com>
2023-09-28 17:17:12 +0800

    how-to：使用SpringBoot技术栈整合Netty构建Websocket应用

---
+++ integration/netty-stack/src/main/java/com/example/NettyStackApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyStackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NettyStackApplication.class, args);
+	}
+}
