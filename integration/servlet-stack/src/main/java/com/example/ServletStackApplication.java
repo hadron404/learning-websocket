a20fa78fed014cddcf07b0de1e3f2662236417fc
zhouqiang <2902511578@qq.com>
2023-09-28 10:49:06 +0800

    重命名模块为servlet-stack

--- integration/servlet-stack/src/main/java/com/example/ServletStackApplication.java
+++ integration/servlet-stack/src/main/java/com/example/ServletStackApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ServletStackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ServletStackApplication.class, args);
 	}
 }
