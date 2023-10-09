1032fb173994f01352e3fcb77f8f473f1c4d74ae
zhouqiang <2902511578@qq.com>
2023-10-09 17:07:07 +0800

    整理项目结构：gradle项目去除根目录的相关配置，避免新建子项目时idea会自动创建空src目录

--- integration/netty-stack/src/main/java/com/example/InboundHandler.java
+++ integration/netty-stack/src/main/java/com/example/InboundHandler.java
@@ -6,10 +6,11 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 public class InboundHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
 		//获取客户端发送的消息内容
 		String text = msg.text();
 		System.out.println(InboundHandler.class + "收到客户端发送来的消息:  " + text);
+		// 将消息发送到下一个handler，按照Initializer注册顺序
 		ctx.fireChannelRead(msg);
 	}
 }
