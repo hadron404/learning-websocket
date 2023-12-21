40f0f2dcee3afb56eec3b2e6a4e70849d5ff856d
zhouqiang <2902511578@qq.com>
2023-12-21 09:03:54 +0800

    how-to: 解决IllegalReferenceCountException

--- integration/netty-stack/src/main/java/com/example/InboundHandler.java
+++ integration/netty-stack/src/main/java/com/example/InboundHandler.java
@@ -9,23 +9,28 @@ import org.springframework.web.socket.PingMessage;
 
 public class InboundHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg)	 {
+	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
 		//获取客户端发送的消息内容
-		String text = msg.text();
-		System.out.println(InboundHandler.class + "收到客户端发送来的消息:  " + text);
+		System.out.println(InboundHandler.class + "收到客户端发送来的消息:  " + msg);
+		msg.content().retain();
 		// 将消息发送到下一个handler，按照Initializer注册顺序
 		ctx.fireChannelRead(msg);
 	}
 
 	@Override
-	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-		if (evt instanceof IdleStateEvent) {
-			IdleStateEvent e = (IdleStateEvent) evt;
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
+		if (evt instanceof IdleStateEvent e) {
 			if (e.state() == IdleState.READER_IDLE) {
 				ctx.close();
 			} else if (e.state() == IdleState.WRITER_IDLE) {
 				ctx.writeAndFlush(new PingMessage());
 			}
 		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("捕捉到异常：" + cause.getMessage());
+		ctx.close();
 	}
 }
